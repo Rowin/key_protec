c228044acd83f2ff0f4bbd8af1fcb39b856e036f
rowin <rowin@anhydride-acide.fr>
2021-06-24 20:02:26 +0200

    Réorganisation des dépôts

--- pico/card_read.py
+++
@@ -1,9 +0,0 @@
-from wiegand import Wiegand
-from machine import Pin
-
-wiegand_one = Pin(27, Pin.IN, Pin.PULL_DOWN)
-
-while True:
-    print(wiegand_one.value())
-
-# wiegand_one.irq(handler=lambda _: print("IRQ"), trigger=Pin.IRQ_FALLING)
